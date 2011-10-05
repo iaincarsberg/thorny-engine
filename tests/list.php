6b9e168daf1135021db0c8a808008dbb7688c8dc
Iain Carsberg <horrain@gmail.com>
2011-10-05 23:25:40 +0100

    TODO generate a list of all specs within the tests directory

--- tests/list.php
+++ tests/list.php
@@ -8,11 +8,13 @@ header("Cache-Control: max-age=0, no-cache, no-store, must-revalidate");
 header("Pragma: no-cache");
 header("Expires: Wed, 11 Jan 1984 05:00:00 GMT");
 
-
-?>
+/*
 /tests/core/common/math/poly2.spec.js
 /tests/core/common/math/vector2.spec.js
 /tests/core/common/observer/observable-observer.spec.js
 /tests/core/common/observer/observable.spec.js
 /tests/core/common/observer/observer.spec.js
+/tests/thorny.spec.js
+*/
+?>
 /tests/thorny.spec.js
