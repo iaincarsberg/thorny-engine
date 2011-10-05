87663abde51a1855976284939e5dc5e2d2af8596
Iain Carsberg <horrain@gmail.com>
2011-10-05 23:25:09 +0100

    Created a common unit test file, run-jasmine.js is used by the server and client to execute the unit tests

--- run-jasmine.js
+++ run-jasmine.js
@@ -1,114 +1,87 @@
-/*global require global*/
-require.paths.push(__dirname + "/lib/jasmine-node/lib/jasmine-node");
-require.paths.push(__dirname);
+/*global window global jasmine*/
+// Node.js
+if (typeof window === 'undefined') {
+	var 
+		requirejs        = require('./lib/r'),
+		fs               = require('fs'),
+		jasminePath      = './lib/jasmine-node/lib/jasmine-node/jasmine-2.0.0.rc1',
+		jasmineReporter  = './lib/jasmine-amd/consoleJasmineReporter2',
+		findAllUnitTests = function (directory, files) {
+			if (files === undefined) {
+				files = [];
+			}
+			
+			var path, stats, key, contents = fs.readdirSync(directory);
+			
+			for (key in contents) {
+				if (contents.hasOwnProperty(key)) {
+					path = [directory, contents[key]].join('/');
+					stats = fs.statSync(path);
+					
+					if (stats.isDirectory()) {
+						findAllUnitTests(path, files);
+						
+					} else if (
+						stats.isFile(path) &&
+						path.substr(-8) === '.spec.js'
+					) {
+						files.push(path);
+					}
+				}
+			}
+			
+			return files;
+		};
+	
+	requirejs.config({
+		nodeRequire: require,
+	});
+	
+	//make define available globally like it is in the browser
+	global.define   = require('./lib/r');
+	
+	//make jasmine available globally like it is in the browser
+	global.describe = require(jasminePath).describe;
+	global.it       = require(jasminePath).it;
+	global.expect   = require(jasminePath).expect;
+	global.waitsFor = require(jasminePath).waitsFor;
+	
+	//bring in and list all the tests to be run
+	requirejs(
+		findAllUnitTests('./tests'),
+		function (
+			ModuleSpec
+		) {
+			var 
+				jasmine = require(jasminePath).jasmine,
+				ConsoleJasmineReporter2 = require(jasmineReporter).ConsoleJasmineReporter;
 
-var jasmine = require('index');
-var sys = require('sys'),
-    Path= require('path');
-
-var specFolder = 'tests';
-
-for (var key in jasmine)
-  global[key] = jasmine[key];
-
-var isVerbose = false;
-var showColors = true;
-var teamcity = process.env.TEAMCITY_PROJECT_NAME || false;
-var extentions = "js";
-var match = '.'
-
-var junitreport = {
-  report: false,
-  savePath : "./reports/",
-  useDotNotation: true,
-  consolidate: true
-}
-
-var args = process.argv.slice(2);
-
-while(args.length) {
-  var arg = args.shift();
-
-  switch(arg)
-  {
-    case '--color':
-      showColors = true;
-      break;
-    case '--noColor':
-    case '--nocolor':
-      showColors = false;
-      break;
-    case '--verbose':
-      isVerbose = true;
-      break;
-    case '--coffee':
-      require('coffee-script');
-      extentions = "js|coffee";
-      break;
-    case '-m':
-    case '--match':
-      match = args.shift();
-      break;
-    case '-i':
-    case '--include':
-      var dir = args.shift();
-
-      if(!Path.existsSync(dir))
-        throw new Error("Include path '" + dir + "' doesn't exist!");
-
-      require.paths.unshift(dir);
-      break;
-    case '--junitreport':
-        junitreport.report = true;
-        break;
-    case '--teamcity':
-        teamcity = true;
-        break;
-    default:
-      if (arg.match(/^--/)) help();
-      specFolder = Path.join(process.cwd(), arg);
-      break;
-  }
-}
-
-if (!specFolder) {
-  help();
-}
-
-var exitCode = 0;
-
-process.on("exit", onExit);
-
-function onExit() {
-  process.removeListener("exit", onExit);
-  process.exit(exitCode);
-}
-
-jasmine.loadHelpersInFolder(specFolder, new RegExp("[-_]helper\\.(" + extentions + ")$"));
-jasmine.executeSpecsInFolder(specFolder, function(runner, log){
-  sys.print('\n');
-  if (runner.results().failedCount == 0) {
-    exitCode = 0;
-  } else {
-    exitCode = 1;
-  }
-}, isVerbose, showColors, teamcity, new RegExp(match + "spec\\.(" + extentions + ")$", 'i'), junitreport);
-
-function help(){
-  sys.print([
-    'USAGE: jasmine-node [--color|--noColor] [--verbose] [--coffee] directory'
-  , ''
-  , 'Options:'
-  , '  --color            - use color coding for output'
-  , '  --noColor          - do not use color coding for output'
-  , '  -m, --match REGEXP - load only specs containing "REGEXPspec"'
-  , '  -i, --include DIR  - add given directory to node include paths'
-  , '  --verbose          - print extra information per each test run'
-  , '  --coffee           - load coffee-script which allows execution .coffee files'
-  , '  --junitreport      - export tests results as junitreport xml format'
-  , '  --teamcity         - converts all console output to teamcity custom test runner commands. (Normally auto detected.)'
-  , ''
-  ].join("\n"));
-
-  process.exit(-1);
+			jasmine.getEnv().addReporter(new ConsoleJasmineReporter2());
+			jasmine.getEnv().execute();
+		}
+	);
+	
+// Browser
+} else {
+	require(
+		[
+			'./lib/requirejs/text!./tests/list.php'
+		],
+		function (list) {
+			list = [
+				'./tests/core/common/math/poly2.spec.js',
+				'./tests/core/common/math/vector2.spec.js',
+				'./tests/core/common/observer/observable-observer.spec.js',
+				'./tests/core/common/observer/observable.spec.js',
+				'./tests/core/common/observer/observer.spec.js',
+				'./tests/thorny.spec.js'
+			].join("\n");
+			
+			require(list.split("\n"), function () {
+				//run tests
+				jasmine.getEnv().addReporter(new jasmine.TrivialReporter());
+				jasmine.getEnv().execute();
+			});
+		}
+	);
 }
