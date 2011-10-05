b1d323efbad0bcb8e598c3362c72634f35c07635
Iain Carsberg <horrain@gmail.com>
2011-10-05 16:49:30 +0100

    Because I couldn't get the manifests to execute downloaded javascript, as from the of the tech they are designed to download content, but not execute it, until the browser tells it to, this failed.

---
+++ tests/list.php
@@ -0,0 +1,18 @@
+<?php
+
+// I know, I know PHP sucks right...
+header("Content-type: text/text");
+
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: max-age=0, no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: Wed, 11 Jan 1984 05:00:00 GMT");
+
+
+?>
+/tests/core/common/math/poly2.spec.js
+/tests/core/common/math/vector2.spec.js
+/tests/core/common/observer/observable-observer.spec.js
+/tests/core/common/observer/observable.spec.js
+/tests/core/common/observer/observer.spec.js
+/tests/thorny.spec.js
