7e196911ffa2cdce1ea2a6ea175a7b2cdc1d3dad
Iain Carsberg <horrain@gmail.com>
2011-10-07 19:57:38 +0100

    Added a fancy ascii banner to the test runner, encase people forget they are using thorny engine :P

--- run-jasmine.js
+++ run-jasmine.js
@@ -1,11 +1,19 @@
-/*global window global jasmine*/
+/*global console window global jasmine*/
 // Node.js
 if (typeof window === 'undefined') {
+	console.log(" _   _                                               _            ");
+	console.log("| |_| |__   ___  _ __ _ __  _   _    ___ _ __   __ _(_)_ __   ___ ");
+	console.log("| __| '_ \\ \/ _ \\| '__| '_ \\| | | |  / _ \\ '_ \\ / _` | | '_ \\ / _ \\");
+	console.log("| |_| | | | (_) | |  | | | | |_| | |  __/ | | | (_| | | | | |  __/");
+	console.log(" \\__|_| |_|\\___/|_|  |_| |_|\\__, |  \\___|_| |_|\\__, |_|_| |_|\\___|");
+	console.log("                            |___/              |___/              ");
+	console.log('Test Suite');
 	var 
 		requirejs        = require('./lib/r'),
 		fs               = require('fs'),
 		jasminePath      = './lib/jasmine-node/lib/jasmine-node/jasmine-2.0.0.rc1',
 		jasmineReporter  = './lib/jasmine-amd/consoleJasmineReporter2',
+		printedOnRow     = 0,
 		findAllUnitTests = function (directory, files) {
 			if (files === undefined) {
 				files = [];
@@ -43,7 +51,18 @@ if (typeof window === 'undefined') {
 	//make jasmine available globally like it is in the browser
 	global.describe = require(jasminePath).describe;
 	global.it       = require(jasminePath).it;
-	global.expect   = require(jasminePath).expect;
+	global.expect   = function (actual) {
+		// Fudge in a dot for each assertition.
+		printedOnRow += 1;
+		process.stdout.write('.');
+		
+		if (printedOnRow === 66) {
+			process.stdout.write("\n");
+			printedOnRow = 0;
+		}
+		
+		return require(jasminePath).expect(actual);
+	};
 	global.waitsFor = require(jasminePath).waitsFor;
 	
 	//bring in and list all the tests to be run
@@ -55,7 +74,7 @@ if (typeof window === 'undefined') {
 			var 
 				jasmine = require(jasminePath).jasmine,
 				ConsoleJasmineReporter2 = require(jasmineReporter).ConsoleJasmineReporter;
-
+			
 			jasmine.getEnv().addReporter(new ConsoleJasmineReporter2());
 			jasmine.getEnv().execute();
 		}
