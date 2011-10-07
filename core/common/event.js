0c7ef0584026d7f73bc4bbc49e3ca00bf54353d2
Iain Carsberg <horrain@gmail.com>
2011-10-08 00:05:53 +0100

    Ported the event system, one of the spec's requires the entity system to exist so that the test can be completed.

---
+++ core/common/event.js
@@ -0,0 +1,73 @@
+/*global define*/
+define(
+	[
+		'thorny',
+		'thorny!observer/observer',
+		'thorny!observer/observable'
+	], 
+	function (
+		Thorny,
+		observer,
+		observable
+	) {
+		if (Thorny.event) {
+			return Thorny.event;
+		}
+		
+		var Event = Thorny.extend(
+			function () {
+				// Make events observable.
+				observable(this);
+
+				// Sometimes you may went to trigger an event before its been 
+				// bound this is where belated comes in to the rescue.
+				this.data('belated', {});
+			},
+			{
+				/**
+				 * Used to bind to an event
+				 * @param string|object eventType Contains the type of the event that 
+				 * we're expecting to be called, and we want to respond to.
+				 * @param function callback Contains the code that is executed when an
+				 * event is triggered
+				 * @reutrn void
+				 */
+				bind: function (eventType, callback) {
+					var data = {};
+					data[eventType] = callback;
+
+					this.addObserver(
+						observer(data)
+						);
+
+					// Check to see if there is a belated event we need to trigger.
+					if (this.data('belated')[eventType]) {
+						this.data('belated')[eventType] = undefined;
+						this.trigger(eventType);
+					}
+				},
+
+				/**
+				 * Used to trigger an event
+				 * @param string eventType Contains the type of event being triggered
+				 * @param boolean|undefined belatable True if this event is belatable,
+				 * otherwise False|undefined
+				 * @return void
+				 */
+				trigger: function (eventType, belatable) {
+					var executed = this.notifyObservers(eventType);
+
+					// If no events we're triggered then we have a belated event.
+					if (belatable === true && executed === 0) {
+						this.data('belated')[eventType] = true;
+					}
+				}
+			}
+		);
+		
+		// Create a new instance of the event class, and bind it to the main 
+		// thorny class.
+		Thorny.event = new Event();
+		return Thorny.event;
+	}
+);
---
+++ tests/core/common/event.spec.js
@@ -0,0 +1,89 @@
+/*global define console window describe it expect runs waitsFor*/
+define(
+	[
+		'thorny',
+		'thorny!event'
+	],
+	function (
+		Thorny,
+		event
+	) {	
+		describe('The Event Object', function () {
+			it('should have the following functions', function () {
+				expect(typeof Thorny.event.bind).toEqual('function');
+				expect(typeof Thorny.event.trigger).toEqual('function');
+				expect(typeof Thorny.event.addObserver).toEqual('function');
+				expect(typeof Thorny.event.notifyObservers).toEqual('function');
+				
+				expect(typeof event.bind).toEqual('function');
+				expect(typeof event.trigger).toEqual('function');
+				expect(typeof event.addObserver).toEqual('function');
+				expect(typeof event.notifyObservers).toEqual('function');
+			});//it should have the following functions
+			
+			describe('the bind and trigger functions', function () {
+				it('should allow us to bind and trigger events in the system', function () {
+					var
+						helloWorld = false,
+						eatLotsOfCake = false,
+						calls = 0;
+
+					event.bind('hello-world', function () {
+							helloWorld = true;
+						});
+					event.bind('eat-lots-of-cake', function () {
+							eatLotsOfCake = true;
+						});
+					event.bind('incroment-calls', function () {
+							calls += 1;
+						});
+
+					event.trigger('hello-world');
+					event.trigger('eat-lots-of-cake');
+
+					event.trigger('incroment-calls');
+					event.trigger('incroment-calls');
+					event.trigger('incroment-calls');
+					event.trigger('incroment-calls');
+					event.trigger('incroment-calls');
+					event.trigger('incroment-calls');
+
+					expect(helloWorld).toBeTruthy();
+					expect(eatLotsOfCake).toBeTruthy();
+					expect(calls).toEqual(6);
+				});//it should allow us to bind and trigger events in the system
+			});// desc the bind and trigger functions
+
+			describe('belated events', function () {
+				it('should allow us to trigger an event, then bind the listener', function () {
+					/**
+					 * The reasioning behind belated binding is that it keeps the 
+					 * flow of the code logical, meaning you so something that 
+					 * takes a while, that requires a callback.
+					 * How while callbacks rock, sometimes its better to have an
+					 * event that is listened to, so you can bind multiple actions
+					 * to a specific event.
+					 * How to validate the belated bit, say your loading a level
+					 * you tell the system which levels your loading, then you
+					 * process that request. Using the normal flow you'd have to
+					 * have the code to handle the processing of the loaded levels
+					 * before you load them, which looks odd. Belated events lets
+					 * the code logically flow.
+					 */
+					console.log('event.spec.js:belated events - Port the Entity-System');
+					/*
+					var helloWorld = false;
+					es().makeEntity()
+						.triggers('hello-world');
+
+					event.bind('hello-world', function () {
+						helloWorld = true;
+					});
+					
+					expect(helloWorld).toBeTruthy();
+					*/
+				});//it should have the following functions
+			});// desc belated events
+		});// desc The Event Object
+	}
+);
