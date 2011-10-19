0f8c5cfffb728d8ceb2d5f6502ab313caf93c686
Iain Carsberg <horrain@gmail.com>
2011-10-19 21:46:15 +0100

    Added a tile2 maths module, this should be enough to form the bases for a 2d cell based map

---
+++ core/common/math/tile2.js
@@ -0,0 +1,168 @@
+/*global define*/
+define(
+	[
+		'thorny',
+		'thorny!math/vector2',
+		'compose',
+		'cjs!underscore'
+	], 
+	function (
+		Thorny,
+		Vector2,
+		Compose,
+		underscore
+	) {
+		var 
+			Tile2,
+			defaults = {
+				width: 32,
+				height: 32
+			};
+		
+		Tile2 = Thorny.extend(
+			function (x, y, width, height) {
+				if (width === undefined) {
+					width = defaults.width;
+				}
+				if (height === undefined) {
+					height = defaults.height;
+				}
+				
+				// Half the width and height
+				width /= 2;
+				height /= 2;
+				
+				var 
+					i,// Used for loop control
+					centroid;
+				
+				// Find the centroid of the tile
+				centroid = new Vector2(x + width, y + height);
+				
+				// Mixin the centroid
+				Compose.call(
+					this,
+					centroid
+					);
+				
+				// Store the height and width
+				this.data('width', width);
+				this.data('height', height);
+				
+				// Build the vector2 data store.
+				this.data('vector2s', [
+					new Vector2(width * -1, height * -1),
+					new Vector2(width, height * -1),
+					new Vector2(width, height),
+					new Vector2(width * -1, height)
+				]);
+			},
+			{
+				/**
+				 * Used to access the Vector2s witin the node.
+				 * @param void
+				 * @return array Contains the raw polys nodes
+				 */
+				getVector2s: function () {
+					var 
+						vectors = this.data('vector2s'),
+						response = [],
+						i, ii;
+					
+					for (i = 0, ii = vectors.length; i < ii; i += 1) {
+						response.push(
+							vectors[i]
+								.clone()
+								.add(this)
+							);
+					}
+					
+					return response;
+				},
+				
+				/**
+				 * Used to get the midpoint of a poly.
+				 * We can directly return references to the midpoint
+				 * as the vector2 exposes no way to alter the
+				 * internal value.
+				 * @param void
+				 * @return vector2 Containing the midpoint of the poly2
+				 */
+				getMidpoint: function () {
+					return this;
+				},
+				
+				/**
+				 * Used to see if two polys share a common edge.
+				 * @param poly2 poly2 Contains a remove poly
+				 * @return boolean true is edge is shared
+				 */
+				sharesEdge: function (tile2) {
+					if (tile2 instanceof Tile2) {
+						if (this.sameAs(tile2)) {
+							return false;
+						}
+						
+						if (this.getX() === tile2.getX() &&
+							(
+								this.getY() === tile2.getY() - this.data('height') - tile2.data('height') ||
+								this.getY() === tile2.getY() + this.data('height') + tile2.data('height')
+							)
+						) {
+							return true;
+							
+						} else if (
+							this.getY() === tile2.getY() &&
+							(
+								this.getX() === tile2.getX() - this.data('width') - tile2.data('width') ||
+								this.getX() === tile2.getX() + this.data('width') + tile2.data('width')
+							)
+						) {
+							return true;
+						}
+					}
+					
+					return false;
+				},
+				
+				/**
+				 * Used to see if a point is within this shape.
+				 * @param object point Contains a vector2
+				 * @return boolean
+				 * @url http://mathworld.wolfram.com/TriangleInterior.html
+				 * @note thanks to footyfish and andrewjbaker for spending a
+				 * lonely wednesday everning in april helping me get this 
+				 * working right.
+				 */
+				isVector2Internal: function (point) {
+					if (
+						(
+							point.getX() >= this.getX() - this.data('width') &&
+							point.getX() <= this.getX() + this.data('width') 
+						) && (
+							point.getY() >= this.getY() - this.data('height') &&
+							point.getY() <= this.getY() + this.data('height')
+						)
+					) {
+						return true;
+					}
+					
+					return false;
+				}
+			}
+		);
+		
+		/**
+		 * Used to set the defaults for objects of type Tile2
+		 * @param object options Contains Tile2 options
+		 * @return this, to allow object chaining
+		 */
+		Tile2.setDefaults = function (options) {
+			defaults = underscore.merge(defaults, options);
+			return Tile2;
+		};
+		
+		// Make the Tile2d available to the big wide world!
+		return Tile2;
+	}
+);
---
+++ tests/core/common/math/tile2.spec.js
@@ -0,0 +1,257 @@
+/*global define console window describe it expect runs waitsFor*/
+define(
+	[
+		'thorny!math/tile2',
+		'thorny!math/vector2'
+	],
+	function (
+		Tile2,
+		Vector2
+	) {	
+		describe('The tile2 object', function () {
+			describe('before instantiation', function () {
+				it('should have a number of helper functions, and Compose.js helpers', function () {
+					expect(typeof Tile2).toEqual('function');// Because it has a constructer
+					expect(typeof Tile2.setDefaults).toEqual('function');
+				});// it should have a number of helper functions, and Compose.js helpers
+			});// desc before instantiation
+			
+			describe('once instantiated', function () {
+				it('it should have the following functions', function () {
+					var t2 = new Tile2(0, 0);
+					
+					expect(typeof t2.getVector2s).toEqual('function');
+					expect(typeof t2.getMidpoint).toEqual('function');
+					expect(typeof t2.sharesEdge).toEqual('function');
+					expect(typeof t2.isVector2Internal).toEqual('function');
+				});// it should have the following functions
+				
+				describe('has the following function,', function () {
+					describe('getVector2s', function () {
+						it('it should allow us to create a small 8x8 tile', function () {
+							var 
+								t2 = new Tile2(0, 0, 8, 8),
+								v2s = t2.getVector2s();
+							expect(v2s.length).toEqual(4);
+							expect(typeof v2s[0]).toEqual('object');
+							expect(typeof v2s[1]).toEqual('object');
+							expect(typeof v2s[2]).toEqual('object');
+							expect(typeof v2s[3]).toEqual('object');
+							
+							expect(v2s[0].getSimpleCoords()).toEqual([0, 0]);
+							expect(v2s[1].getSimpleCoords()).toEqual([8, 0]);
+							expect(v2s[2].getSimpleCoords()).toEqual([8, 8]);
+							expect(v2s[3].getSimpleCoords()).toEqual([0, 8]);
+							
+						});// it should allow us to create a small 8x8 tile
+						
+						it('it should allow us to create a small 8x16 tile', function () {
+							var 
+								t2 = new Tile2(0, 0, 8, 16),
+								v2s = t2.getVector2s();
+							expect(v2s.length).toEqual(4);
+							expect(typeof v2s[0]).toEqual('object');
+							expect(typeof v2s[1]).toEqual('object');
+							expect(typeof v2s[2]).toEqual('object');
+							expect(typeof v2s[3]).toEqual('object');
+							
+							expect(v2s[0].getSimpleCoords()).toEqual([0, 0]);
+							expect(v2s[1].getSimpleCoords()).toEqual([8, 0]);
+							expect(v2s[2].getSimpleCoords()).toEqual([8, 16]);
+							expect(v2s[3].getSimpleCoords()).toEqual([0, 16]);
+							
+						});// it should allow us to create a small 8x16 tile
+						
+						it('it should allow us to create a small 16x8 tile', function () {
+							var 
+								t2 = new Tile2(0, 0, 16, 8),
+								v2s = t2.getVector2s();
+							expect(v2s.length).toEqual(4);
+							expect(typeof v2s[0]).toEqual('object');
+							expect(typeof v2s[1]).toEqual('object');
+							expect(typeof v2s[2]).toEqual('object');
+							expect(typeof v2s[3]).toEqual('object');
+							
+							expect(v2s[0].getSimpleCoords()).toEqual([0, 0]);
+							expect(v2s[1].getSimpleCoords()).toEqual([16, 0]);
+							expect(v2s[2].getSimpleCoords()).toEqual([16, 8]);
+							expect(v2s[3].getSimpleCoords()).toEqual([0, 8]);
+							
+						});// it should allow us to create a small 8x8 tile
+					});// desc getVector2s
+					
+					describe('getMidpoint', function () {
+						it('it should find the centroid in a 16x16 tile', function () {
+							var 
+								t2 = new Tile2(0, 0, 16, 16),
+								v2 = t2.getMidpoint();
+							expect(v2.getSimpleCoords()).toEqual([8, 8]);
+						});// it should find the centroid in a 16x16 tile
+						
+						it('it should find the centroid in a 32x16 tile', function () {
+							var 
+								t2 = new Tile2(0, 0, 32, 16),
+								v2 = t2.getMidpoint();
+							expect(v2.getSimpleCoords()).toEqual([16, 8]);
+						});// it should find the centroid in a 32x16 tile
+						
+						it('it should find the centroid in a non zeroed 32x32 tile', function () {
+							var 
+								t2 = new Tile2(96, 0, 32, 32),
+								v2 = t2.getMidpoint();
+							expect(v2.getSimpleCoords()).toEqual([112, 16]);
+						});// it should find the centroid in a non zeroed 32x32 tile
+					});// desc getMidpoint
+					
+					describe('sharesEdge', function () {
+						it('it should find which tiles share an edge', function () {
+							var t1, t2, t3, t4, t5, t6, t7, t8, t9;
+
+							t1 = new Tile2(0, 0, 8, 8);
+							t2 = new Tile2(8, 0, 8, 8);
+							t3 = new Tile2(16, 0, 8, 8);
+							t4 = new Tile2(0, 8, 8, 8);
+							t5 = new Tile2(8, 8, 8, 8);
+							t6 = new Tile2(16, 8, 8, 8);
+							t7 = new Tile2(0, 16, 8, 8);
+							t8 = new Tile2(8, 16, 8, 8);
+							t9 = new Tile2(16, 16, 8, 8);
+
+							// Tile 1 vs
+							expect(t1.sharesEdge(t2)).toBeTruthy();
+							expect(t1.sharesEdge(t3)).toBeFalsy();
+							expect(t1.sharesEdge(t4)).toBeTruthy();
+							expect(t1.sharesEdge(t5)).toBeFalsy();
+							expect(t1.sharesEdge(t6)).toBeFalsy();
+							expect(t1.sharesEdge(t7)).toBeFalsy();
+							expect(t1.sharesEdge(t8)).toBeFalsy();
+							expect(t1.sharesEdge(t9)).toBeFalsy();
+
+							// Tile 2 vs
+							expect(t2.sharesEdge(t1)).toBeTruthy();
+							expect(t2.sharesEdge(t3)).toBeTruthy();
+							expect(t2.sharesEdge(t4)).toBeFalsy();
+							expect(t2.sharesEdge(t5)).toBeTruthy();
+							expect(t2.sharesEdge(t6)).toBeFalsy();
+							expect(t2.sharesEdge(t7)).toBeFalsy();
+							expect(t2.sharesEdge(t8)).toBeFalsy();
+							expect(t2.sharesEdge(t9)).toBeFalsy();
+
+							// Tile 3 vs
+							expect(t3.sharesEdge(t1)).toBeFalsy();
+							expect(t3.sharesEdge(t2)).toBeTruthy();
+							expect(t3.sharesEdge(t4)).toBeFalsy();
+							expect(t3.sharesEdge(t5)).toBeFalsy();
+							expect(t3.sharesEdge(t6)).toBeTruthy();
+							expect(t3.sharesEdge(t7)).toBeFalsy();
+							expect(t3.sharesEdge(t8)).toBeFalsy();
+							expect(t3.sharesEdge(t9)).toBeFalsy();
+
+							// Tile 4 vs
+							expect(t4.sharesEdge(t1)).toBeTruthy();
+							expect(t4.sharesEdge(t2)).toBeFalsy();
+							expect(t4.sharesEdge(t3)).toBeFalsy();
+							expect(t4.sharesEdge(t5)).toBeTruthy();
+							expect(t4.sharesEdge(t6)).toBeFalsy();
+							expect(t4.sharesEdge(t7)).toBeTruthy();
+							expect(t4.sharesEdge(t8)).toBeFalsy();
+							expect(t4.sharesEdge(t9)).toBeFalsy();
+
+							// Tile 5 vs
+							expect(t5.sharesEdge(t1)).toBeFalsy();
+							expect(t5.sharesEdge(t2)).toBeTruthy();
+							expect(t5.sharesEdge(t3)).toBeFalsy();
+							expect(t5.sharesEdge(t4)).toBeTruthy();
+							expect(t5.sharesEdge(t6)).toBeTruthy();
+							expect(t5.sharesEdge(t7)).toBeFalsy();
+							expect(t5.sharesEdge(t8)).toBeTruthy();
+							expect(t5.sharesEdge(t9)).toBeFalsy();
+
+							// Tile 6 vs
+							expect(t6.sharesEdge(t1)).toBeFalsy();
+							expect(t6.sharesEdge(t2)).toBeFalsy();
+							expect(t6.sharesEdge(t3)).toBeTruthy();
+							expect(t6.sharesEdge(t4)).toBeFalsy();
+							expect(t6.sharesEdge(t5)).toBeTruthy();
+							expect(t6.sharesEdge(t7)).toBeFalsy();
+							expect(t6.sharesEdge(t8)).toBeFalsy();
+							expect(t6.sharesEdge(t9)).toBeTruthy();
+
+							// Tile 7 vs
+							expect(t7.sharesEdge(t1)).toBeFalsy();
+							expect(t7.sharesEdge(t2)).toBeFalsy();
+							expect(t7.sharesEdge(t3)).toBeFalsy();
+							expect(t7.sharesEdge(t4)).toBeTruthy();
+							expect(t7.sharesEdge(t5)).toBeFalsy();
+							expect(t7.sharesEdge(t6)).toBeFalsy();
+							expect(t7.sharesEdge(t8)).toBeTruthy();
+							expect(t7.sharesEdge(t9)).toBeFalsy();
+
+							// Tile 8 vs
+							expect(t8.sharesEdge(t1)).toBeFalsy();
+							expect(t8.sharesEdge(t2)).toBeFalsy();
+							expect(t8.sharesEdge(t3)).toBeFalsy();
+							expect(t8.sharesEdge(t4)).toBeFalsy();
+							expect(t8.sharesEdge(t5)).toBeTruthy();
+							expect(t8.sharesEdge(t6)).toBeFalsy();
+							expect(t8.sharesEdge(t7)).toBeTruthy();
+							expect(t8.sharesEdge(t9)).toBeTruthy();
+
+							// Tile 9 vs
+							expect(t9.sharesEdge(t1)).toBeFalsy();
+							expect(t9.sharesEdge(t2)).toBeFalsy();
+							expect(t9.sharesEdge(t3)).toBeFalsy();
+							expect(t9.sharesEdge(t4)).toBeFalsy();
+							expect(t9.sharesEdge(t5)).toBeFalsy();
+							expect(t9.sharesEdge(t6)).toBeTruthy();
+							expect(t9.sharesEdge(t7)).toBeFalsy();
+							expect(t9.sharesEdge(t8)).toBeTruthy();
+						});// it should find which tiles share an edge
+					});// desc sharesEdge
+					
+					describe('isVector2Internal', function () {
+						it('it should detect internal vectors in a zeroed 8x8 Tile2', function () {
+							expect(new Tile2(0, 0, 8, 8).isVector2Internal(new Vector2(0, 0))).toBeTruthy();
+							expect(new Tile2(0, 0, 8, 8).isVector2Internal(new Vector2(0, 8))).toBeTruthy();
+							expect(new Tile2(0, 0, 8, 8).isVector2Internal(new Vector2(8, 8))).toBeTruthy();
+							expect(new Tile2(0, 0, 8, 8).isVector2Internal(new Vector2(8, 0))).toBeTruthy();
+							expect(new Tile2(0, 0, 8, 8).isVector2Internal(new Vector2(4, 4))).toBeTruthy();
+							expect(new Tile2(0, 0, 8, 8).isVector2Internal(new Vector2(5, 3))).toBeTruthy();
+						});// it should detect internal vectors in a zeroed 8x8 Tile2
+						
+						it("it shouldn't detect internal vectors in a zeroed 8x8 Tile2", function () {
+							expect(new Tile2(0, 0, 8, 8).isVector2Internal(new Vector2(-1, 0))).toBeFalsy();
+							expect(new Tile2(0, 0, 8, 8).isVector2Internal(new Vector2(0, 9))).toBeFalsy();
+							expect(new Tile2(0, 0, 8, 8).isVector2Internal(new Vector2(8, 9))).toBeFalsy();
+							expect(new Tile2(0, 0, 8, 8).isVector2Internal(new Vector2(8, -1))).toBeFalsy();
+							expect(new Tile2(0, 0, 8, 8).isVector2Internal(new Vector2(14, 4))).toBeFalsy();
+							expect(new Tile2(0, 0, 8, 8).isVector2Internal(new Vector2(5, 13))).toBeFalsy();
+						});// it shouldn't detect internal vectors in a zeroed 8x8 Tile2
+						
+						it('it should detect internal vectors in a zeroed 32x32 Tile2', function () {
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(0, 0))).toBeTruthy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(0, 32))).toBeTruthy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(32, 32))).toBeTruthy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(32, 0))).toBeTruthy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(4, 4))).toBeTruthy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(23, 12))).toBeTruthy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(14, 19))).toBeTruthy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(29, 32))).toBeTruthy();
+						});// it should detect internal vectors in a zeroed 8x8 Tile2
+						
+						it("it shouldn't detect internal vectors in a zeroed 32x32 Tile2", function () {
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(-1, 0))).toBeFalsy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(0, 33))).toBeFalsy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(32, 33))).toBeFalsy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(32, -1))).toBeFalsy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(4, 34))).toBeFalsy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(23, 212))).toBeFalsy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(154, 19))).toBeFalsy();
+							expect(new Tile2(0, 0, 32, 32).isVector2Internal(new Vector2(129, 32))).toBeFalsy();
+						});// it shouldn't detect internal vectors in a zeroed 8x8 Tile2
+					});// desc isVector2Internal
+				});// desc has the following function,
+			});// desc once instantiated
+		});// desc The tile2 object
+	}
+);
